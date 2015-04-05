7f3510f69570d9faa943993c17d763d091536b03
Scott <scotthowells@hotmail.com>
2015-04-05 11:37:16 -0400

    Previous Laptop Work
    Basic GUI

---
+++ src/www/CSE410/homework4/com/test.java
@@ -0,0 +1,28 @@
+/**
+ * test.java
+ * By: J.E.Scott Howells
+ * Description: 
+ * Created:
+ * Modified: 
+ */
+package www.CSE410.homework4.com;
+
+/**
+ * @author Scott Howells
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 * Description:
+	 * Parameters
+	 * Return:
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OntologyGui ont = new OntologyGui();
+		ont.buildWindow();
+	}
+
+}
