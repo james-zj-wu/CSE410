c2e64bcd5a63be43a059289324627079e17b5213
Scott <howells.jes@gmail.com>
2015-04-06 19:55:25 -0400

    Got out to be in scroll pane
    nicely output the screen output

--- bin/www/CSE410/homework4/com/OntologyGui.java
+++ bin/www/CSE410/homework4/com/OntologyGui.java
@@ -15,7 +15,10 @@ import javax.swing.*;
 
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.query.* ;
+
 import java.io.*;
 /**
  * @author Scott Howells
@@ -36,37 +39,47 @@ public class OntologyGui{
 	
 	//Output Area
 	private JPanel outputArea = new JPanel(new GridLayout(0,1));
+	private JTextArea opTextArea = new JTextArea();
 	private JPanel out = new JPanel();
-	private JTextArea output = new JTextArea();
-	private JScrollPane outputScroll = new JScrollPane(output);
+	private JScrollPane outputScroll = new JScrollPane();
+
+	TextAreaPrintStream outputStream;//redirect to my text area
 	/**
 	 * Method: buildWindow
 	 * Description: Constructs frame work for the GUI
 	 */
 	public void buildWindow(){
-		//Build the basic frame
 		
+		//Build the basic frame		
 		ontWindow.setLayout(new GridLayout(0,1));
         ontWindow.setSize(1000,500);
+        ontWindow.setResizable(false);
         ontWindow.setLocationRelativeTo(null);
         ontWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	   
-		//Divide window into two areas for layout purposes
-		JPanel searchArea = new JPanel(new GridLayout(0,3));
-		ontWindow.add(searchArea);
-		ontWindow.add(outputArea);
+	  
+	    //Divide window into two areas for layout purposes
+	    JPanel searchArea = new JPanel(new GridLayout(0,3));
+	    ontWindow.add(searchArea);
+	    ontWindow.add(outputArea);
+	    
+	    //Set Up Output pane
+	    this.out.setMaximumSize(new Dimension(outputArea.getMaximumSize().width,outputArea.getMinimumSize().height));
+	    this.out.setLayout(new BoxLayout(out, BoxLayout.Y_AXIS));
+  		this.opTextArea.setColumns(10);
+  		this.opTextArea.setRows(5);
+  		this.opTextArea.setEditable(false);
+  		this.opTextArea.setLineWrap(true);
+  		this.opTextArea.setWrapStyleWord(true);
+  		this.outputStream =new TextAreaPrintStream(opTextArea);
+  		outputStream.println("Test");
+
 		
 		//Organise Search Area for multiple options
 		JPanel ButtonArea = new JPanel(new FlowLayout());
 		JPanel SQLArea = new JPanel(new GridLayout(1,3));
 		JPanel SearchBoxArea = new JPanel(new FlowLayout());
 		
-		//Set Up Output pane
-		this.output.setColumns(10);
-		this.output.setRows(5);
-		this.output.setLineWrap(true);
-		this.output.setWrapStyleWord(true);
-		this.outputScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		
 		
 		//Set Max Size of Panels
 		ButtonArea.setMaximumSize(new Dimension((searchArea.getMaximumSize().width/3),(searchArea.getMaximumSize().height)));
@@ -86,7 +99,7 @@ public class OntologyGui{
 		SQLSearchBT.addActionListener(SPARQLHandler);
 		searchWord.addActionListener(searchHandler);
 		
-		//Buttons for output area
+		//Buttons for output display
 		JPanel buttonInnerBox = new JPanel(new GridLayout(0,1));
 
 		
@@ -101,18 +114,16 @@ public class OntologyGui{
 		JPanel SQLInnerBox = new JPanel();
 		SQLInnerBox.setLayout(new BoxLayout(SQLInnerBox, BoxLayout.Y_AXIS));
 		JLabel SQLWordLabel = new JLabel("SPARQL Queries");
-		this.SQLSearch = new JTextArea("PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n" +
-				"SELECT ?url\n " +
-				"WHERE {" +
-				" ?contributor foaf:name \"John Barstow\" . " +
-				" ?contributor foaf:weblog ?url . " +
-				" }",5,15);//"PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n SELECT \n WHERE{} \n");
+		this.SQLSearch = new JTextArea("PREFIX owl:<http://www.w3.org/2002/07/owl#>\n" +
+				"SELECT ?url ?name\n " +
+				"WHERE {?url owl:sameIndividualAs ?name"  +
+				" }",5,15);//"PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n PREFIX owl:<http://www.w3.org/2002/07/owl#>\nSELECT \n WHERE{} \n");
 		
 		this.SQLSearch.setColumns(10);
 		this.SQLSearch.setRows(5);
 		this.SQLSearch.setLineWrap(true);
 		this.SQLSearch.setWrapStyleWord(true);
-		JScrollPane SQLScrollWindow =new JScrollPane(this.SQLSearch);
+		JScrollPane SQLScrollWindow = new JScrollPane(this.SQLSearch);
 		SQLScrollWindow.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		//Add Display Layout Buttons
@@ -140,7 +151,10 @@ public class OntologyGui{
 				
 		//Decorate Output Area
 		outputArea.setBorder(BorderFactory.createLineBorder(Color.black));
-		outputArea.setBackground(Color.white);
+		out.setBackground(Color.WHITE);
+		outputScroll.setViewportView(opTextArea); //Make the text area scroll able
+		outputArea.add(out.add(outputScroll));
+		outputArea.updateUI();
 
 		//Show window
 		ontWindow.setVisible(true);
@@ -188,20 +202,44 @@ public class OntologyGui{
 
 		public void actionPerformed(ActionEvent event) {
 			//initiate Output area
-			out.removeAll();
-			out.setLayout(new BoxLayout(out, 1));
-			output.setText(null);
-			out.add(outputScroll);
-			out.updateUI();
+
+			outputArea.removeAll();
+			opTextArea.setText(null);
 			
 			if(event.getSource()== showOnt ){
-				System.out.println("Show Ontology");//TODO:REMOVE THIS
+				
+				String inputFileName = "./ontology/food.owl";
+				outputStream.println("Show Ontology");//TODO:REMOVE THIS
+				
+				OntModel model = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM, null );
+				// use the FileManager to open the bloggers RDF graph from the filesystem
+				InputStream in = FileManager.get().open(inputFileName);
+				if (in == null) {
+					throw new IllegalArgumentException( "File: " + inputFileName + " not found");
+				}
+				// read the RDF/XML file
+				model.read( in, "" );
+				new ClassHierarchy().showHierarchy( outputStream, model );
+				
+				//display stuff
+				outputScroll.setViewportView(opTextArea);
+				outputArea.add(out.add(outputScroll));
+				outputArea.updateUI();
+				
 			}
 			else if (event.getSource()== showEntities ){
-				System.out.println("Show Entity");//TODO:REMOVE THIS
+				outputStream.println("Show Entity");//TODO:REMOVE THIS
+				//display stuff
+				outputScroll.setViewportView(opTextArea);
+				outputArea.add(out.add(outputScroll));
+				outputArea.updateUI();
 			}
 			else if (event.getSource()== showSubClasses ){
-				System.out.println("Show Sub Classes");//TODO:REMOVE THIS	
+				outputStream.println("Show Sub Classes");//TODO:REMOVE THIS	
+				//display stuff
+				outputScroll.setViewportView(opTextArea);
+				outputArea.add(out.add(outputScroll));//add stuff back
+				outputArea.updateUI();
 			}
 			else if (event.getSource()== queryResults ){
 				SQLSearchBT.doClick();//why reinvent the wheel :)
@@ -218,7 +256,10 @@ public class OntologyGui{
 	private class SearchHandler implements ActionListener{
 
 		public void actionPerformed(ActionEvent event) {
-			System.out.println("Searched "+wordSearch.getText());//TODO:REMOVE THIS		
+			outputStream.println("Searched "+wordSearch.getText());//TODO:REMOVE THIS	
+			//display stuff
+			//outputArea.add(out.add(outputScroll.add(opTextArea)));//add stuff back
+			outputArea.updateUI();
 		}
 	}
 	/**
@@ -231,19 +272,20 @@ public class OntologyGui{
 	private class SPARQLSearchHandler implements ActionListener{
 		
 		public void actionPerformed(ActionEvent event) {
-			System.out.println("Searched "+SQLSearch.getText());//TODO:REMOVE THIS	
-			OntologyTutorial5 ontology = new OntologyTutorial5(SQLSearch.getText());
+			String inputFileName = "./ontology/food.owl";
+			outputStream.println("Searched "+SQLSearch.getText());//TODO:REMOVE THIS	
+			OntologyTutorial5 ontology = new OntologyTutorial5(SQLSearch.getText(),inputFileName);
 		}
 		
 	}
 
 	private class OntologyTutorial5 {
-		static final String inputFileName = "./ontology/bloggers.rdf";
-		public OntologyTutorial5 (String queryString){
+		
+		public OntologyTutorial5 (String queryString,String inputFileName){
 			//initiate Output area
-			output.setText(null); //clear last result
-			out.removeAll();
-			out.setLayout(new BoxLayout(out, 1));
+			outputArea.removeAll();
+			opTextArea.setText(null);
+			
 			
 			// Create an empty in-memory model
 			Model model = ModelFactory.createDefaultModel();
@@ -255,23 +297,23 @@ public class OntologyGui{
 			// read the RDF/XML file
 			model.read( in, "" );
 			//Create a new query
-
 			Query query = QueryFactory.create(queryString);
 			//Execute the query and obtain results
 			QueryExecution qe = QueryExecutionFactory.create(query, model);
 			ResultSet results = qe.execSelect();
+			
 			//Convert Results to string
 			String resultString = ResultSetFormatter.asText(results);
-			//Output results
-			output.setText(resultString);
-			//Display results
-			out.add(outputScroll);
-			outputArea.add(out);
-			System.out.println(resultString);//TODO Remove This
-			//Important -free up resources used running the query
-			out.updateUI();
-			ontWindow.revalidate();
 			
+			//Output results
+			outputStream.println(resultString);
+			
+			//Display results
+			outputScroll.setViewportView(opTextArea);
+			outputArea.add(out.add(outputScroll));
+			outputArea.updateUI();
+
+			//Important -free up resources used running the query
 			qe.close();
 		}
 	}
