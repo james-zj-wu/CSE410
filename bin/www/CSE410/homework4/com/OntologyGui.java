3f3f2d10c56c2c65a49cfb864965a88144276776
Scott <howells.jes@gmail.com>
2015-04-06 13:55:04 -0400

    Basic actions working and SPARQL queries Framwork
    - Set up default actions (doesn't do anything yet for most)
    - Got basic SPARQL to work just needs to be converted to our ontology

--- bin/www/CSE410/homework4/com/OntologyGui.java
+++ bin/www/CSE410/homework4/com/OntologyGui.java
@@ -15,21 +15,39 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.StrokeBorder;
 
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.query.* ;
+import java.io.*;
 /**
  * @author Scott Howells
  *
  */
 public class OntologyGui{
 	
+	//Fields used across functions
+	JFrame ontWindow = new JFrame("Ontology Window");
+	private JTextField wordSearch;
+	private JTextArea SQLSearch;
+	private JButton showOnt =new JButton("Show Ontology");
+	private JButton showEntities = new JButton ("Show Entities");
+	private JButton showSubClasses	= new JButton("Show Sub Classes");
+	private JButton queryResults	= new JButton("Show Query Results");
+	private JButton searchWord =new JButton("Search");
+	private JButton SQLSearchBT =new JButton("Search");
+	
+	//Output Area
+	private JPanel outputArea = new JPanel(new GridLayout(0,1));
+	private JPanel out = new JPanel();
+	private JTextArea output = new JTextArea();
+	private JScrollPane outputScroll = new JScrollPane(output);
 	/**
-	 * 
-	 * Description:
-	 * Parameters
-	 * Return:
+	 * Method: buildWindow
+	 * Description: Constructs frame work for the GUI
 	 */
 	public void buildWindow(){
 		//Build the basic frame
-		JFrame ontWindow = new JFrame("Ontology Window");
+		
 		ontWindow.setLayout(new GridLayout(0,1));
         ontWindow.setSize(1000,500);
         ontWindow.setLocationRelativeTo(null);
@@ -37,15 +55,20 @@ public class OntologyGui{
 	   
 		//Divide window into two areas for layout purposes
 		JPanel searchArea = new JPanel(new GridLayout(0,3));
-		JPanel outputArea = new JPanel(new GridLayout(0,1));
 		ontWindow.add(searchArea);
 		ontWindow.add(outputArea);
 		
-		//TODO:Build Options
 		//Organise Search Area for multiple options
 		JPanel ButtonArea = new JPanel(new FlowLayout());
 		JPanel SQLArea = new JPanel(new GridLayout(1,3));
 		JPanel SearchBoxArea = new JPanel(new FlowLayout());
+		
+		//Set Up Output pane
+		this.output.setColumns(10);
+		this.output.setRows(5);
+		this.output.setLineWrap(true);
+		this.output.setWrapStyleWord(true);
+		this.outputScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		//Set Max Size of Panels
 		ButtonArea.setMaximumSize(new Dimension((searchArea.getMaximumSize().width/3),(searchArea.getMaximumSize().height)));
@@ -53,27 +76,46 @@ public class OntologyGui{
 		SearchBoxArea.setMaximumSize(new Dimension((searchArea.getMaximumSize().width/3),(searchArea.getMaximumSize().height)));
 		
 		//Create Interfaces
+		//Handlers
+		DisplayEventHandler displayAreaHandler = new DisplayEventHandler();
+		SPARQLSearchHandler SPARQLHandler = new SPARQLSearchHandler();
+		SearchHandler searchHandler = new SearchHandler();
+		
+		showOnt.addActionListener(displayAreaHandler);
+		showEntities.addActionListener(displayAreaHandler);
+		showSubClasses.addActionListener(displayAreaHandler);
+		queryResults.addActionListener(displayAreaHandler);
+		SQLSearchBT.addActionListener(SPARQLHandler);
+		searchWord.addActionListener(searchHandler);
+		
 		//Buttons for output area
 		JPanel buttonInnerBox = new JPanel(new GridLayout(0,1));
+
 		
-		JButton showOnt =new JButton("Show Ontology");
-		JButton showEntities = new JButton ("Show Entities");
-		JButton showSubClasses	= new JButton("Show Sub Classes");
-		JButton queryResults	= new JButton("Show Query Results");
 		//Search for Entities
 		JPanel searchInnerBox = new JPanel();
 		searchInnerBox.setLayout(new BoxLayout(searchInnerBox, BoxLayout.Y_AXIS));
 		JLabel searchWordLabel = new JLabel("Search Entities");
-		JTextField wordSearch = new JTextField("",8);
-		wordSearch.setMaximumSize(new Dimension(SearchBoxArea.getMaximumSize().width,wordSearch.getMinimumSize().height));
-		JButton searchWord =new JButton("Search");
+		this.wordSearch = new JTextField("",8);
+		this.wordSearch.setMaximumSize(new Dimension(SearchBoxArea.getMaximumSize().width,wordSearch.getMinimumSize().height));
+
 		//Search for Entities
 		JPanel SQLInnerBox = new JPanel();
 		SQLInnerBox.setLayout(new BoxLayout(SQLInnerBox, BoxLayout.Y_AXIS));
 		JLabel SQLWordLabel = new JLabel("SPARQL Queries");
-		JTextArea SQLSearch = new JTextArea("SELECT \n WHERE{} \n ORDER BY \n");
-		SQLSearch.setMaximumSize(new Dimension(SQLArea.getMaximumSize().width,SQLSearch.getMinimumSize().height));
-		JButton SQLSearchBT =new JButton("Search");
+		this.SQLSearch = new JTextArea("PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n" +
+				"SELECT ?url\n " +
+				"WHERE {" +
+				" ?contributor foaf:name \"John Barstow\" . " +
+				" ?contributor foaf:weblog ?url . " +
+				" }",5,15);//"PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n SELECT \n WHERE{} \n");
+		
+		this.SQLSearch.setColumns(10);
+		this.SQLSearch.setRows(5);
+		this.SQLSearch.setLineWrap(true);
+		this.SQLSearch.setWrapStyleWord(true);
+		JScrollPane SQLScrollWindow =new JScrollPane(this.SQLSearch);
+		SQLScrollWindow.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		//Add Display Layout Buttons
 		ButtonArea.add(buttonInnerBox);
@@ -90,7 +132,7 @@ public class OntologyGui{
 		
 		SQLArea.add(SQLInnerBox);
 		SQLInnerBox.add(SQLWordLabel);
-		SQLInnerBox.add(SQLSearch);
+		SQLInnerBox.add(SQLScrollWindow);
 		SQLInnerBox.add(SQLSearchBT);
 		
 		//Add All Divisions		
@@ -136,6 +178,97 @@ public class OntologyGui{
 		}
 		return temp;
 	}
+	
+	/**
+	 * OntologyGui
+	 * By: J.E.Scott Howells
+	 * Description: 
+	 * Created:Apr 6, 2015
+	 * Modified: 
+	 */
+	private class DisplayEventHandler implements ActionListener{
 
+		public void actionPerformed(ActionEvent event) {
+			
+			if(event.getSource()== showOnt ){
+				System.out.println("Show Ontology");//TODO:REMOVE THIS
+			}
+			else if (event.getSource()== showEntities ){
+				System.out.println("Show Entity");//TODO:REMOVE THIS
+			}
+			else if (event.getSource()== showSubClasses ){
+				System.out.println("Show Sub Classes");//TODO:REMOVE THIS	
+			}
+			else if (event.getSource()== queryResults ){
+				System.out.println("Show Query Results");//TODO:REMOVE THIS
+			}
+			
+			
+		}
+	}
+	/**
+	 * OntologyGui
+	 * By: J.E.Scott Howells
+	 * Description: 
+	 * Created:Apr 6, 2015
+	 * Modified: 
+	 */
+	private class SearchHandler implements ActionListener{
 
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Searched "+wordSearch.getText());//TODO:REMOVE THIS		
+		}
+	}
+	/**
+	 * OntologyGui
+	 * By: J.E.Scott Howells
+	 * Description: 
+	 * Created:Apr 6, 2015
+	 * Modified: 
+	 */
+	private class SPARQLSearchHandler implements ActionListener{
+		
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Searched "+SQLSearch.getText());//TODO:REMOVE THIS	
+			OntologyTutorial5 ontology = new OntologyTutorial5(SQLSearch.getText());
+		}
+		
+	}
+
+	private class OntologyTutorial5 {
+		static final String inputFileName = "./ontology/bloggers.rdf";
+		public OntologyTutorial5 (String queryString){
+			//initiate Output area
+			output.setText(null); //clear last result
+			out.removeAll();
+			out.setLayout(new BoxLayout(out, 1));
+			
+			// Create an empty in-memory model
+			Model model = ModelFactory.createDefaultModel();
+			// use the FileManager to open the bloggers RDF graph from the filesystem
+			InputStream in = FileManager.get().open(inputFileName);
+			if (in == null) {
+				throw new IllegalArgumentException( "File: " + inputFileName + " not found");
+			}
+			// read the RDF/XML file
+			model.read( in, "" );
+			//Create a new query
+
+			Query query = QueryFactory.create(queryString);
+			//Execute the query and obtain results
+			QueryExecution qe = QueryExecutionFactory.create(query, model);
+			ResultSet results = qe.execSelect();
+			//Convert Results to string
+			String resultString = ResultSetFormatter.asText(results);
+			//Output results
+			output.setText(resultString);
+			//Display results
+			out.add(outputScroll);
+			outputArea.add(out);
+			System.out.println(resultString);//TODO Remove This
+			//Important -free up resources used running the query
+			ontWindow.revalidate();
+			qe.close();
+		}
+	}
 }
